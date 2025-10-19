253b28591c9aedf66379b5fc27cacda96b1c0d26
japleen-arora77 <arorajapleen.cs2277@gmail.com>
2025-10-20 00:59:38 +0530

    otherCities-forecast-activated

--- src/components/forecastCard.tsx
+++ src/components/forecastCard.tsx
@@ -1,10 +1,28 @@
-import React from "react";
-import {forecastDay} from "../types/weatherTypes";
+import React, { useEffect, useState } from "react";
+import { forecastDay, weatherData } from "../types/weatherTypes";
+import getWeather from "../api/weatherAPI"; 
+
 interface ForecastProps{
     forecast : forecastDay[];
 }
+const otherCities=["Vancouver","Melbourne","Delhi","Bangalore","Pune","New York" ];
 const ForecastCard: React.FC<ForecastProps> = ({forecast}) => {
-    
+    const [otherCitiesWeather, setOtherCitiesWeather] = useState<weatherData[]>([]);
+    const [loadingCities, setLoadingCities] = useState(true);
+
+    useEffect(()=>{
+        const fetchOtherCities=async()=>{
+            const res:weatherData[] = [];
+            for(const i of otherCities){
+                const data=await getWeather(i);
+                if(data) res.push(data.current);
+            }
+            setOtherCitiesWeather(res);
+            setLoadingCities(false);
+        };
+        fetchOtherCities();
+    })
+
     return(
         <div className="forecast-card container ">
             <div className="forecast-card-body p-4">
@@ -16,71 +34,54 @@ const ForecastCard: React.FC<ForecastProps> = ({forecast}) => {
                                 <h3 className="h3-type head">Today/Week</h3>
                                 <br/>
                                 <div className="row today-forecast p-2 m-2">
-                                    {/* {forecast[0]?.hour.slice(12,18).map((hourData,index)=>(
-                                            <div className="col-lg-2 show-temperature">
-                                            <p className="sub-head">
-                                            {new Date(hourData.time_epoch * 1000).getHours() % 12 || 12}
-                                            {new Date(hourData.time_epoch * 1000).getHours() >= 12 ? "PM" : "AM"}
-                                            </p>
-                                            <p><img src={hourData.condition.icon} alt={hourData.condition.text} width="40"/></p>
-                                            <p className="sub-head">{Math.round(hourData.temp_c)}°C</p>
-                                            </div>
-                                    ))
-                                    } */}
                                         {(() => {  //IIFE function exp
                                              if (!forecast[0]?.hour) return null;
                                              const currentLocalTime = new Date().getHours();
-
-  // get current local time in hours (0–23)
-                                            //const now = new Date().getHours();
-
-  // create a list of next 5 hours (wrapping around 24)
                                             const nextFiveHours: number[] = [];
-                                            for (let i = 1; i <= 6; i++) {
+                                            for (let i = 1; i <= 6; i++) { //forecast for futher 6 hours 
                                              nextFiveHours.push((currentLocalTime + i) % 24);
                                             }
-                                            // Find corresponding forecast data for those 5 hours
-                      const hourDataList = nextFiveHours
-                      .map((targetHour) =>
-                        forecast[0].hour.find(
-                          (h: any) =>
-                            new Date(h.time).getHours() === targetHour
-                        )
-                      )
-                      .filter(Boolean); // remove undefined if missing data
-                      const filledHourData =
-                      hourDataList.length < 5
-                        ? [
-                            ...hourDataList,
-                            ...forecast[0].hour.slice(0, 5 - hourDataList.length),
-                          ]
-                        : hourDataList;
+                                        const hourDataList = nextFiveHours
+                                        .map((targetHour) =>
+                                          forecast[0].hour.find(
+                                            (h: any) =>
+                                              new Date(h.time).getHours() === targetHour
+                                          )
+                                        )
+                                        .filter(Boolean);
+                                        const filledHourData =
+                                        hourDataList.length < 5
+                                          ? [
+                                              ...hourDataList,
+                                              ...forecast[0].hour.slice(0, 5 - hourDataList.length),
+                                            ]
+                                         : hourDataList;
   // filter the hours from the API based on those 5 hour values
                                           
-  return filledHourData.map((hourData: any, index: number) => (
-    <div className="col-lg-2 show-temperature" key={index}>
-      <p className="sub-head">
-        {(() => {
-          const hour = new Date(hourData.time).getHours();
-          const formattedHour = hour % 12 || 12;
-          const ampm = hour >= 12 ? "PM" : "AM";
-          return `${formattedHour}${ampm}`;
-        })()}
-      </p>
-      <p>
-        <img
-          src={hourData.condition.icon}
-          alt={hourData.condition.text}
-          width="40"
-        />
-      </p>
-      <p className="sub-head">
-        {Math.round(hourData.temp_c)}°C
-      </p>
-    </div>
-  ));
-})()}
-</div>
+                            return filledHourData.map((hourData: any, index: number) => (
+                                 <div className="col-lg-2 show-temperature" key={index}>
+                                      <p className="sub-head">
+                                        {(() => {
+                                          const hour = new Date(hourData.time).getHours();
+                                          const formattedHour = hour % 12 || 12;
+                                          const ampm = hour >= 12 ? "PM" : "AM";
+                                          return `${formattedHour}${ampm}`;
+                                        })()}
+                                     </p>
+                                      <p>
+                                        <img
+                                          src={hourData.condition.icon}
+                                          alt={hourData.condition.text}
+                                         width="40"
+                                        />
+                                      </p>
+                                      <p className="sub-head">
+                                        {Math.round(hourData.temp_c)}°C
+                                      </p>
+                                    </div>
+                                ));
+                                })()}
+                                </div>
                                 <div className="row further-days-forecast p-2 m-2">
                                     <div className="row">
                                         <div className="col-lg-8">
@@ -93,7 +94,6 @@ const ForecastCard: React.FC<ForecastProps> = ({forecast}) => {
                                         </div>
                                     </div>
                                 </div>
-
                             </div>
                             <div className="col-lg-4 day-length p-3 ">
                                 <p className="sub-head">Sunrise</p>
@@ -111,62 +111,63 @@ const ForecastCard: React.FC<ForecastProps> = ({forecast}) => {
                     <div className="col-lg-5 text-start">
                     <div className="other-cities ">   
                     <h3 className="h3-type head">Other Cities</h3>
-                        <div className="row p-3">
-                            <div className="col-lg-6">
-                                <div className="other-city-data p-3 ">
-                                <div className="row">
-                                    <div className="col-lg-7">
-                                        <h4 className="h4-type head">14℃</h4>
-                                        <p className="sub-head">USA</p>
-                                    </div>
-                                    <div className="col-lg-5 head">
-                                        <h1>🌥️</h1>
-                                    </div>
-                                </div> 
-                                </div>
+                    {loadingCities ? (
+                     <p className="sub-head">Loading cities....</p>
+                    ):( 
+                        <> 
+                    <div className="row p-3">
+                    {otherCitiesWeather.slice(0, 2).map((city, index) => (
+                      <div key={index} className="col-lg-6">
+                        <div className="other-city-data p-3">
+                          <div className="row">
+                            <div className="col-lg-7">
+                              <h4 className="h4-type head">{Math.round(city.temperature)}℃</h4>
+                              <p className="sub-head">{city.city}</p>
                             </div>
-                            <div className="col-lg-6 p-0">
-                            <div className="other-city-data p-3">
-                                <div className="row">
-                                    <div className="col-lg-7">
-                                        <h4 className="h4-type head">27℃</h4>
-                                        <p className="sub-head">Dubai</p>
-                                    </div>
-                                    <div className="col-lg-5">
-                                        <h1>🌥️</h1>
-                                    </div>
-                                </div> 
-                                </div>
+                            <div className="col-lg-5 head">
+                              <img src={city.icon} alt={city.desc} width="40" />
                             </div>
+                          </div>
                         </div>
-                        <div className="row p-3 ">
-                            <div className="col-lg-6 ">
-                            <div className="other-city-data p-3">
-                                <div className="row">
-                                    <div className="col-lg-7">
-                                        <h4 className="h4-type head">16℃</h4>
-                                        <p className="sub-head">China</p>
-                                    </div>
-                                    <div className="col-lg-5">
-                                        <h1>🌥️</h1>
-                                    </div>
-                                </div> 
-                                </div>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="row p-3">
+                    {otherCitiesWeather.slice(2, 4).map((city, index) => (
+                      <div key={index} className="col-lg-6">
+                        <div className="other-city-data p-3">
+                          <div className="row">
+                            <div className="col-lg-7">
+                              <h4 className="h4-type head">{Math.round(city.temperature)}℃</h4>
+                              <p className="sub-head">{city.city}</p>
                             </div>
-                            <div className="col-lg-6 p-0">
-                            <div className="other-city-data p-3">
-                                <div className="row">
-                                    <div className="col-lg-7">
-                                        <h4 className="h4-type head">26℃</h4>
-                                        <p className="sub-head">Canada</p>
-                                    </div>
-                                    <div className="col-lg-5">
-                                        <h1>🌥️</h1>
-                                    </div>
-                                </div> 
-                                </div>
+                            <div className="col-lg-5 head">
+                              <img src={city.icon} alt={city.desc} width="40" />
                             </div>
+                          </div>
                         </div>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="row p-3">
+                    {otherCitiesWeather.slice(4, 6).map((city, index) => (
+                      <div key={index} className="col-lg-6">
+                        <div className="other-city-data p-3">
+                          <div className="row">
+                            <div className="col-lg-7">
+                              <h4 className="h4-type head">{Math.round(city.temperature)}℃</h4>
+                              <p className="sub-head">{city.city}</p>
+                            </div>
+                            <div className="col-lg-5 head">
+                              <img src={city.icon} alt={city.desc} width="40" />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                    </>
+                    )}
                         </div>
                     </div>
                 </div>
